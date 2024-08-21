f5aa5c15d253b2b6867f9306c7fb287f53a175d7
전민오 <jbj338033@gmail.com>
2024-08-21 21:39:16 +0900

    feat: test

--- src/main/kotlin/com/testserver/controller/TestController.kt
+++ src/main/kotlin/com/testserver/controller/TestController.kt
@@ -1,4 +1,10 @@
 package com.testserver.controller
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class TestController {
+    @GetMapping("/test")
+    fun test() = "서버 켜져 있음!"
 }
